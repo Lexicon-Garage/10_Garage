67c8141a894857a1e8a7d72c07a8f60f19cfac53
Marwan Saadeh <marwansaadeh@outlook.com>
2026-07-02 17:02:28 +0200

    Created firstMigration and Database

---
+++ Garage.Web/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Garage.Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Garage.Web.Data
+{
+    public class AppDbConext : DbContext
+    {
+        public AppDbConext(DbContextOptions<AppDbConext> options)
+        : base(options)
+        {
+        }
+        public DbSet<ParkedVehicle> ParkedVehicles 
+        {
+            get; set;
+        }
+
+}
+}
