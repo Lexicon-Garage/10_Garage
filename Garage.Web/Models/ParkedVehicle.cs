f282672e709b08257761dcd96b65beec87425d15
mrdabek <81967634+mrdabek@users.noreply.github.com>
2026-07-06 23:35:24 +0200

    Fix spaces between labels. ie RegistrationNumber

--- Garage.Web/Models/ParkedVehicle.cs
+++ Garage.Web/Models/ParkedVehicle.cs
@@ -9,26 +9,33 @@ namespace Garage.Web.Models
 
         [Required]
         [StringLength(60, MinimumLength = 5)]
+        [Display(Name = "Registration Number")]
         public required string RegistrationNumber { get; set; }
 
         [EnumDataType(typeof(VehicleType))]
+        [Display(Name = "Vehicle Type")]
         public required VehicleType VehicleType { get; set; }
 
         [Required]
         [StringLength(20, MinimumLength = 2)]
+        [Display(Name = "Color")]
         public string? Color { get; set; }
 
         [Range(1, 16)]
+        [Display(Name = "Number of wheels")]
         public required int NumberOfWheels { get; set; }
 
         [Required]
         [StringLength(50, MinimumLength = 1)]
+        [Display(Name = "Model")]
         public string? Model { get; set; }
 
         [EnumDataType(typeof(BrandType))]
+        [Display(Name = "Brand")]
         public required BrandType BrandType { get; set; }
-
+            
         [DataType(DataType.DateTime)]
+        [Display(Name = "Arrived Time")]
         public required DateTime ArrivedTime { get; init; }
     }
 }
