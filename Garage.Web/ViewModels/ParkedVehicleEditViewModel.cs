026603095aef19197f407642d6bf6191399b4cd6
mrdabek <81967634+mrdabek@users.noreply.github.com>
2026-07-07 16:41:12 +0200

    Fix attributes display for edit page, fix spaces

--- Garage.Web/ViewModels/ParkedVehicleEditViewModel.cs
+++ Garage.Web/ViewModels/ParkedVehicleEditViewModel.cs
@@ -10,23 +10,30 @@ namespace Garage.Web.ViewModels
 
         [Required]
         [StringLength(60, MinimumLength = 5)]
-        public string RegistrationNumber { get; set; } = string.Empty;
+        [Display(Name = "Registration Number")]
 
+        public string RegistrationNumber { get; set; } = string.Empty;
         [EnumDataType(typeof(VehicleType))]
+        [Display(Name = "Vehicle Type")]
+
         public VehicleType VehicleType { get; set; }
 
         [Required]
         [StringLength(20, MinimumLength = 2)]
+        [Display(Name = "Color")]
         public string? Color { get; set; }
 
         [Range(1, 16)]
+        [Display(Name = "Number of wheels")]
         public int NumberOfWheels { get; set; }
 
         [Required]
         [StringLength(50, MinimumLength = 1)]
+        [Display(Name = "Model")]
         public string? Model { get; set; }
 
         [EnumDataType(typeof(BrandType))]
+        [Display(Name = "Brand")]
         public BrandType BrandType { get; set; }
 
         public DateTime ArrivedTime { get; set; }
