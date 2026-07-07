1fd260205ef6c8a4361ea177b48868dd8d1aa3fc
mrdabek <81967634+mrdabek@users.noreply.github.com>
2026-07-07 09:53:40 +0200

    Moved attributes from ParkedVehicle.cs to ParkedVehicleDetailsViewModel

--- Garage.Web/ViewModels/ParkedVehicleDetailsViewModel.cs
+++ Garage.Web/ViewModels/ParkedVehicleDetailsViewModel.cs
@@ -8,25 +8,32 @@ public class ParkedVehicleDetailsViewModel
     
     [Required]
     [StringLength(60, MinimumLength = 5)]
+    [Display(Name = "Registration Number")]
     public string RegistrationNumber { get; set; } = string.Empty;
     
     [EnumDataType(typeof(VehicleType))]
+    [Display(Name = "Vehicle Type")]
     public VehicleType VehicleType { get; set; }
     
     [Required]
     [StringLength(20, MinimumLength = 2)]
+    [Display(Name = "Color")]
     public string? Color { get; set; }
     
     [Range(1, 16)]
+    [Display(Name = "Number of wheels")]
     public int NumberOfWheels { get; set; }
     
     [Required]
     [StringLength(50, MinimumLength = 1)]
+    [Display(Name = "Model")]
     public string? Model { get; set; }
     
     [EnumDataType(typeof(BrandType))]
+    [Display(Name = "Brand")]
     public BrandType BrandType { get; set; }
     
+    [Display(Name = "Arrived at")]
     public DateTime ArrivedTime { get; set; }
     
 }
