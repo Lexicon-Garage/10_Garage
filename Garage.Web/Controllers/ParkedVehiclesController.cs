66d31f90028101714832318967051f89abd3158e
mrdabek <81967634+mrdabek@users.noreply.github.com>
2026-07-08 11:56:41 +0200

    Fix error message text in ParkedVehiclesController Checkout

--- Garage.Web/Controllers/ParkedVehiclesController.cs
+++ Garage.Web/Controllers/ParkedVehiclesController.cs
@@ -228,7 +228,7 @@ public class ParkedVehiclesController : Controller
 		}
 		catch (DbUpdateException)
 		{
-			TempData["ValidationMessage"] = "Database error: Could not update the vehicle data. Please try again.";
+			TempData["ValidationMessage"] = "Could not update the vehicle data. Please try again.";
 		}
 
 
