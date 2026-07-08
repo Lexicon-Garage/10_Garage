5a77f3fa59e27d9096316a08cb2fb7e24ebb79f0
Fredric Melbing <fredric.melbing@gmail.com>
2026-07-08 18:22:45 +0200

    Refactory: fixed Regex in Create, Added Interface to Create/Edit ViewModel to use same NormalizeMethod. Changed from nullable to requered Registrationnumber, Color, Model

---
+++ Garage.Web/ViewModels/Interfaces/IVehicleFormModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Garage.Web.ViewModels.Interfaces
+{
+	public interface IVehicleFormModel
+	{
+		string RegistrationNumber { get; set; }
+		string Color { get; set; }
+		string Model { get; set; }
+	}
+}
