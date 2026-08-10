a8ec8b87675f9fc4474da1659a5d2c4f7889724a
mrdabek <81967634+mrdabek@users.noreply.github.com>
2026-08-10 13:16:38 +0200

    Scaffold custom Identity registration page

--- Garage.Web/Program.cs
+++ Garage.Web/Program.cs
@@ -21,6 +21,8 @@ namespace Garage.Web
             builder.Services.AddDbContext<AppDbConext>(options =>
             options.UseSqlServer(
             builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<AppDbConext>();
             builder.Services
 	            .AddDefaultIdentity<ApplicationUser>()
 	            .AddRoles<IdentityRole>()
