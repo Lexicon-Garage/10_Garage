56482037777563c69216b6bed4d739d0188d1cbb
Fredric Melbing <fredric.melbing@gmail.com>
2026-08-11 10:13:02 +0200

    added parkedVehicle

---
+++ Garage.Web/Models/ParkedVehicle.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+
+namespace Garage.Web.Models
+{
+	public class ParkedVehicle
+	{
+		public int Id { get; set; }
+
+		[Required]
+		[StringLength(60, MinimumLength = 5)]
+		public required string RegistrationNumber { get; set; }
+
+		[EnumDataType(typeof(VehicleType))]
+		public required VehicleType VehicleType { get; set; }
+
+		[Required]
+		[StringLength(20, MinimumLength = 2)]
+		public required string Color { get; set; }
+
+		[Range(1, 16)]
+		public required int NumberOfWheels { get; set; }
+
+		[Required]
+		[StringLength(50, MinimumLength = 1)]
+		public required string Model { get; set; }
+
+		[EnumDataType(typeof(BrandType))]
+		public required BrandType BrandType { get; set; }
+
+		[DataType(DataType.DateTime)]
+		public required DateTime ArrivedTime { get; init; }
+	}
+}
